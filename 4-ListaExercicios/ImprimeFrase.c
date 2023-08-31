c51487668df6015b4517603935dacff760db3e3c
Vinicius Tengaten <eu@vnici.us>
2023-08-31 00:14:11 -0300

    removing warnings of `ImprimeFrase.c`

--- 4-ListaExercicios/ImprimeFrase.c
+++ 4-ListaExercicios/ImprimeFrase.c
@@ -14,30 +14,30 @@
 #include <stdio.h>
 #include <string.h>
 void main() {
-    int adicionandoPalavra, qtdePalavras, posUltimaPalavra;
+    int adicionandoPalavra, qtdePalavras, posProxPalavra;
     char frase[50], palavras[25][50];
 
     printf("Digite uma frase: ");
-    gets(frase);
-    strcat(frase, " ");
+    fgets(frase, 50, stdin);
+    frase[strlen(frase) - 1] = ' ';
 
-    qtdePalavras = posUltimaPalavra = 0;
+    qtdePalavras = posProxPalavra = 0;
     adicionandoPalavra = 0;
     for (int i = 0; i < strlen(frase); i++) {
         if (frase[i] != ' ') {
             adicionandoPalavra = 1;
-            palavras[qtdePalavras][i - posUltimaPalavra] = frase[i];
+            palavras[qtdePalavras][i - posProxPalavra] = frase[i];
         } else {
             if (adicionandoPalavra == 1) {
-                palavras[qtdePalavras][i - posUltimaPalavra] = '\0';
+                palavras[qtdePalavras][i - posProxPalavra] = '\0';
                 qtdePalavras++;
                 adicionandoPalavra = 0;
             }
-            posUltimaPalavra = i + 1;
+            posProxPalavra = i + 1;
         }
     }
 
     for (int i = 0; i < qtdePalavras; i++) {
-        printf("\n%s - %d", palavras[i], strlen(palavras[i]));
+        printf("\n%s - %zu", palavras[i], strlen(palavras[i]));
     }
 }
